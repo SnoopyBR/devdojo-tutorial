c787d4cfdc765ccb54ca5585ba59703b59bbcbc4
juliocp <juliocp@weg.net>
2020-08-17 17:21:53 -0300

    Implementado Methods DELETE

--- springboot-essentials/src/main/java/br/com/devdojo/controller/AnimeController.java
+++ springboot-essentials/src/main/java/br/com/devdojo/controller/AnimeController.java
@@ -1,12 +1,15 @@
-package br.com.devdojo.endpoint;
+package br.com.devdojo.controller;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -42,4 +45,16 @@ public class AnimeController {
 		return ResponseEntity.ok(animeFound);
 	}
 
+	@PostMapping
+	public ResponseEntity<Anime> save(@RequestBody Anime anime){
+		return ResponseEntity.ok(animeRepository.save(anime));
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable long id){
+		log.info("Deleting anime with id {}",id);
+		animeRepository.delete(id);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+	
 }
--- springboot-essentials/src/main/java/br/com/devdojo/repository/AnimeRepository.java
+++ springboot-essentials/src/main/java/br/com/devdojo/repository/AnimeRepository.java
@@ -1,22 +1,46 @@
 package br.com.devdojo.repository;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.server.ResponseStatusException;
 
 import br.com.devdojo.model.Anime;
 
 @Repository
 public class AnimeRepository {
 
-	public List<Anime> listAll(){
-		
-		return Arrays.asList(
+	private static List<Anime> animes;
+	
+	static {
+		animes =new ArrayList<Anime>(List.of(
 				new Anime(1, "DBZ"),
 				new Anime(2, "Berserk"),
 				new Anime(3, "Naruto")
+				));
+	}
+	
+	public List<Anime> listAll(){
+		return animes;
+	}
+	
+	public Anime save(Anime anime) {
+		anime.setId(ThreadLocalRandom.current().nextInt(4,100000));
+		animes.add(anime);
+		return anime;
+	}
+
+	public void delete(long id) {
+		animes.remove(animes
+				.stream()
+				.filter(anime -> anime.getId() == id)
+				.findFirst()
+				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"Anime not found"))
 				);
+		
 		
 	}
 	
